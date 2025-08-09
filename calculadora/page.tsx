ac1ca81eb9e2786c16d9851de427d869c633fb58
analauraescobar04-design <analauraescobar04@gmail.com>
2025-08-09 15:19:15 -0300

    Add files via upload

---
+++ calculadora/page.tsx
@@ -0,0 +1,10 @@
+import CorrelativasCalculator from "../correlativas-calculator"
+
+export default function CalculadoraPage() {
+  return (
+    <div>
+      <h1>Calculadora de Correlativas</h1>
+      <CorrelativasCalculator />
+    </div>
+  )
+}
